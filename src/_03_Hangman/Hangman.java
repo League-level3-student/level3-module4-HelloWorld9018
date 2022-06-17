871c961bd5787e029033a782492608ba5066ed53
Lily Cov <lily@bettersites.com>
2022-06-16 18:31:59 -0700

    Aligned

--- src/_03_Hangman/Hangman.java
+++ src/_03_Hangman/Hangman.java
@@ -1,8 +1,13 @@
 package _03_Hangman;
 
+import java.awt.Component;
 import java.awt.Dimension;
+import java.io.IOException;
 import java.util.Stack;
 
+import javax.imageio.ImageIO;
+import javax.swing.BoxLayout;
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
@@ -13,7 +18,7 @@ public class Hangman {
 
 	public void setup(Utilities utilities) {
 		
-		String rounds = JOptionPane.showInputDialog("Enter the number of rounds you would like to play (1-266): ");
+		String rounds = JOptionPane.showInputDialog("Enter the number of rounds you would like to attempt (1-266): ");
 		int r = Integer.parseInt(rounds);
 		Stack<String> words = new Stack<String>();
 		
@@ -34,20 +39,47 @@ public class Hangman {
 		JFrame frame = new JFrame();
 		frame.setVisible(true);
 		JPanel panel = new JPanel();
+		JLabel image = new JLabel();
 		JLabel label = new JLabel();
+		JLabel space = new JLabel();
+		
+		panel.setLayout(new BoxLayout(panel, BoxLayout.PAGE_AXIS));
 		
 		frame.add(panel);
+		panel.add(image);
+		panel.add(space);
 		panel.add(label);
-		frame.setSize(new Dimension(300, 400));
+		
+		frame.setSize(new Dimension(250, 170));
 		
 		String text = "";
+		
 		for(int i = 0; i< word.length(); i++) {
 			text = text + "_ ";
 		}
 		//add images
+		/*if() {
+			
+		}*/
+		image.setIcon(loadImage(0));
+		image.setAlignmentX(Component.CENTER_ALIGNMENT);
+		//frame.pack();
+		
 		label.setText(text);
+		label.setAlignmentX(Component.CENTER_ALIGNMENT);
+		space.setText("  ");
+		//frame.pack();
 	}
 
 	
+	public static ImageIcon loadImage(int num) {
+		String hangmanfile = "Hangman_" + num + ".jpg";
+		try {
+			return new ImageIcon(ImageIO.read(new Hangman().getClass().getResourceAsStream(hangmanfile)));
+		} catch (IOException e) {
+
+			return null;
+		}
+	}
 	
 }
