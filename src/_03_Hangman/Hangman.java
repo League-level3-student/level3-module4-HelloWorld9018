e9754b5850112270c84c72ddfdb47d616b780602
Lily Cov <lily@bettersites.com>
2022-07-14 18:29:26 -0700

    Just need to clean up the sysos

--- src/_03_Hangman/Hangman.java
+++ src/_03_Hangman/Hangman.java
@@ -21,12 +21,14 @@ import javax.swing.JTextField;
 public class Hangman implements KeyListener {
 	
 	Stack<String> words = new Stack<String>();
-	StringBuilder text = new StringBuilder("");
+	StringBuilder text;
 	String word = "";
 	JTextField textfield;
 	JLabel incorrect;
 	String incorrectLetters;
 	JLabel letters;
+	JLabel image;
+	JFrame frame;
 	int hp;
 	
 	
@@ -42,9 +44,11 @@ public class Hangman implements KeyListener {
 			while(words.contains(random)) {
 				random = utilities.readRandomLineFromFile("dictionary.txt");
 			}
-			
+			//System.out.println(random);
 			words.add(random);
+			
 		}
+		System.out.println(words);
 		word = words.pop();
 		createDisplay(word);
 	}
@@ -52,16 +56,19 @@ public class Hangman implements KeyListener {
 	JLabel blanks = new JLabel();
 	
 	 void createDisplay(String word) {
+		 
+		 text = new StringBuilder("");
+		 
 		 System.out.println(word);
 		 hp = 6;
 		 incorrectLetters = "";
 		 
-		JFrame frame = new JFrame();
+		frame = new JFrame();
 		frame.setVisible(true);
 		JPanel masterPanel = new JPanel();
 		JPanel middlePanel = new JPanel();
 		JPanel bottomPanel = new JPanel();
-		JLabel image = new JLabel();
+		image = new JLabel();
 		letters = new JLabel();
 		incorrect = new JLabel();
 		JPanel topPanel = new JPanel();
@@ -90,15 +97,13 @@ public class Hangman implements KeyListener {
 		frame.setSize(new Dimension(250, 200));
 		
 		
-		
+		System.out.println("brown word: "+ word);
 		for(int i = 0; i< word.length(); i++) {
+			
 			text = text.append("_ ");
 					//+ "_ ";
 		}
-		//add images
-		/*if() {
-			
-		}*/
+		
 		image.setIcon(loadImage(6));
 		image.setAlignmentX(Component.CENTER_ALIGNMENT);
 		
@@ -112,7 +117,7 @@ public class Hangman implements KeyListener {
 	
 	public ImageIcon loadImage(int num) {
 		String hangmanfile = "Hangman_" + num + ".jpg";
-		System.out.println(hangmanfile);
+		//System.out.println(hangmanfile);
 		try {
 			return new ImageIcon(ImageIO.read(new Hangman().getClass().getResourceAsStream(hangmanfile)));
 		} catch (IOException e) {
@@ -126,11 +131,43 @@ public class Hangman implements KeyListener {
 		blanks.setText(text.toString());
 		incorrect.setText(" HP: " + hp);
 		letters.setText("<html> Incorrect: <br/>" + incorrectLetters + "</html>");
+		image.setIcon(loadImage(hp));
+		if(hp == 0) {
+			System.out.println("GAME OVER DEAD");
+		
+		  int response = JOptionPane.showOptionDialog(frame, "Would you like to play again?", "Game Over", JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE, loadImage(hp), null, 0);
+		  if(response == 0) {
+			  //yes
+			  frame.dispose();
+			  Utilities utilities = new Utilities();
+			  setup(utilities);
+			  
+		  }
+		  else {
+			  //no
+			  frame.dispose();
+		  }
+		}
+		else if(!text.toString().contains("_ ")) {
+			
+			
+			frame.dispose();
+			if(words.size()>0) {
+				JOptionPane.showMessageDialog(frame, "GREAT JOB!  \nWord: " + word + " \nClick OK to proceed to the next round");
+				System.out.println(words);
+				word = words.pop();
+				System.out.println("word:  " + word);
+				createDisplay(word);
+			}
+			else {
+				JOptionPane.showMessageDialog(frame, "CONGRATULATIONS! You won all the rounds.");
+				//System.out.println("NO MORE WORDS!!!!");
+			}
+			//System.out.println("NEXT ACTION");
+		}
 	}
 	
-	public void updateScores () {
-		
-	}
+
 	
 	public void addAll (Container container, Component... components) {
 		for(int i  = 0; i <components.length; i++) {
@@ -143,23 +180,23 @@ public class Hangman implements KeyListener {
 	public void keyTyped(KeyEvent e) {
 		// TODO Auto-generated method stub
 		boolean correct = false;
-		System.out.println("yes1: " + e.getKeyChar());
+		//System.out.println("yes1: " + e.getKeyChar());
 		char[] wordChar = word.toCharArray();
 		
-		System.out.println(wordChar.length);
+		//System.out.println(wordChar.length);
 		for(int i = 0; i<wordChar.length; i++) {
-			System.out.println("hi: "+ wordChar[i]);
+			//System.out.println("hi: "+ wordChar[i]);
 			String c = "" + e.getKeyChar();
 		if(e.getKeyChar()==(wordChar[i])){
 			correct = true;
-			System.out.println("yes2");
+			//System.out.println("yes2");
 			
-			System.out.println("letter: " + e.getKeyChar());
+			//System.out.println("letter: " + e.getKeyChar());
 			
 			
 			text.replace(i*2, (i*2)+1, c);
 
-			System.out.println("text: " + text);
+			//System.out.println("text: " + text);
 	
 		}
 	
